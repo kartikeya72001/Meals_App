3ad67c4e5aaa4044949b8d3516b14be4dc29ba5a
Kartikeya72001 <kartikeya72001@gmail.com>
2021-08-26 15:29:02 +0530

    HomeScreenSetUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,26 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Meals"),
-      ),
-      body: Center(
-        child: Text("Navigation Time"),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
