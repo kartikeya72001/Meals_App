92ef3981c28794655f91c91395e8e8baf81504d8
Kartikeya72001 <kartikeya72001@gmail.com>
2021-08-27 15:28:11 +0530

    SolvedPageStacking

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawe.dart';
 
 class FilterScreen extends StatelessWidget {
   static const routeName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Filters"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Filter Screen"),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text("Filters"),
+      ),
     );
   }
 }
